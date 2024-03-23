b65f3aeb9c586dc8a6dae6b43e89eaa79d34f87b
arkediy <artur32827@gmail.com>
2024-03-23 16:48:48 +0200

    retry

--- H_Work5_2.py
+++ H_Work5_2.py
@@ -1,24 +1,17 @@
 from typing import Callable
+import re
 
-def is_float(s: str) -> bool:
-    try:
-        float(s)
-        return True
-    except ValueError:
-        return False
-    
 def generator_numbers(text: str):
-    words = text.split()
-    for word in words:
-        if is_float(word):
-            yield float(word)
+    pattern = r'\b\d+\.\d+\b'
+    for match in re.finditer(pattern, text):
+        yield float(match.group())
 
-def sum_profit(text: str, func: Callable[[str], Callable]):
+def sum_profit(text: str, func: Callable):
     numbers_generator = func(text)
     total = sum(numbers_generator)
     return total
 
 
-text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+text = "Загальний дохід працівника складається з декількох частин:1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
 total_income = sum_profit(text, generator_numbers)
 print(f"Загальний дохід: {total_income}")
