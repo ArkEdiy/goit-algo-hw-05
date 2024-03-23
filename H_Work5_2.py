093ffcb0a6571afae2c9ed5e9d657fa3d9b3ad71
arkediy <artur32827@gmail.com>
2024-03-23 16:55:03 +0200

    retry

--- H_Work5_2.py
+++ H_Work5_2.py
@@ -3,8 +3,9 @@ import re
 
 def generator_numbers(text: str):
     pattern = r'\b\d+\.\d+\b'
-    for match in re.finditer(pattern, text):
-        yield float(match.group())
+    numbers = re.findall(pattern, text)
+    for number in numbers:
+        yield float(number)
 
 def sum_profit(text: str, func: Callable):
     numbers_generator = func(text)
@@ -12,6 +13,6 @@ def sum_profit(text: str, func: Callable):
     return total
 
 
-text = "Загальний дохід працівника складається з декількох частин:1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
 total_income = sum_profit(text, generator_numbers)
 print(f"Загальний дохід: {total_income}")
